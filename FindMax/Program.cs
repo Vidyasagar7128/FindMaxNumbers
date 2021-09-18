3babb13036c53d0f4d8549d0e25c5bd10aefb5da
vidyasagar gaikwad <vidyasagargaikwad7128@gmail.com>
2021-09-18 08:14:44 +0530

    Added : First Commit

---
+++ FindMax/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FindMax
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Find MAximum number between Three!");
+        }
+    }
+}
